e789f8a32354747b1ce0b09bc19eb7dc9049e5c5
Salvador de la cruz <sdlchava@gmail.com>
2021-06-09 16:58:31 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+gsap.from('.logo',{opacity: 0, duration: 1, delay:1, x:50})
+gsap.from('.nav__item',{opacity: 0, duration: 1.3, delay:1.2, y:25, ease:'expo.out', stagger: .3})
+gsap.from('.textos',{opacity:0,duration:1.5,delay:1,y:80});
+
